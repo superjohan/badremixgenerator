b66030addb99f59bdf9502c8aacf1bf195360f85
Johan Halin <johan@halin.me>
2019-07-05 17:55:47 +0200

    Initial Commit

---
+++ Bad Remix Generator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bad Remix Generator
+//
+//  Created by Johan Halin on 05/07/2019.
+//  Copyright © 2019 Aero Deko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
